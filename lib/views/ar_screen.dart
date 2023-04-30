830fc0ab9d9a90aea692c173842183a762fda01f
ritamk <ritamkonar7@gmail.com>
2023-05-01 02:44:10 +0530

    deleted unrequired models

--- lib/views/ar_screen.dart
+++ lib/views/ar_screen.dart
@@ -1,4 +1,5 @@
 import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:furniture_ar/models/furniture_model.dart';
 import 'package:vector_math/vector_math_64.dart' as vector;
@@ -12,6 +13,7 @@ class ArScreen extends StatefulWidget {
 }
 
 class _ArScreenState extends State<ArScreen> {
+  bool _loading = false;
   ArCoreController? arCoreController;
 
   @override
@@ -25,11 +27,27 @@ class _ArScreenState extends State<ArScreen> {
       appBar: AppBar(
         title: Text(widget.furnitureModel.name ?? ""),
       ),
-      body: ArCoreView(
-        type: ArCoreViewType.STANDARDVIEW,
-        onArCoreViewCreated: _onArCoreViewCreated,
-        enableTapRecognizer: true,
-        enablePlaneRenderer: true,
+      body: Stack(
+        children: [
+          ArCoreView(
+            type: ArCoreViewType.STANDARDVIEW,
+            onArCoreViewCreated: _onArCoreViewCreated,
+            enableTapRecognizer: true,
+            enablePlaneRenderer: true,
+          ),
+          if (_loading)
+            Center(
+              child: CupertinoAlertDialog(
+                content: Column(
+                  children: const [
+                    CupertinoActivityIndicator(color: Colors.black),
+                    SizedBox(height: 5.0),
+                    Text("Loading"),
+                  ],
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
@@ -42,8 +60,7 @@ class _ArScreenState extends State<ArScreen> {
 
   Future _addSphere(ArCoreHitTestResult hit) async {
     final earthShape = ArCoreReferenceNode(
-      objectUrl:
-          "https://raw.githubusercontent.com/ritamk/furniture_ar/main/assets/scene.gltf",
+      objectUrl: widget.furnitureModel.modelLink,
       position: hit.pose.translation,
       rotation: hit.pose.rotation,
       // scale: vector.Vector3.all(hit.distance * 0.01),
@@ -53,8 +70,9 @@ class _ArScreenState extends State<ArScreen> {
   }
 
   void _handleOnPlaneTap(List<ArCoreHitTestResult> hits) {
+    setState(() => _loading = true);
     final hit = hits.first;
-    _addSphere(hit);
+    _addSphere(hit).then((value) => setState(() => _loading = false));
   }
 
   void onTapHandler(String name) {
