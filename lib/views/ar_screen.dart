cff5a5a95c61e0acd537a75faf47f8ed8d0c3faa
ritamk <ritamkonar7@gmail.com>
2023-05-01 01:57:40 +0530

    more changes to ar screen

--- lib/views/ar_screen.dart
+++ lib/views/ar_screen.dart
@@ -46,12 +46,14 @@ class _ArScreenState extends State<ArScreen> {
     // );
 
     final earthShape = ArCoreReferenceNode(
-      object3DFileName: widget.furnitureModel.modelLink,
-      // objectUrl:
-      //     "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf",
+      // object3DFileName: widget.furnitureModel.modelLink,
+      objectUrl:
+          // "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/Duck/glTF/Duck.gltf",
+          "https://raw.githubusercontent.com/ritamk/furniture_ar/main/assets/scene.gltf",
+      // "https://github.com/KhronosGroup/glTF-Sample-Models/raw/master/2.0/CesiumMan/glTF/CesiumMan.gltf",
       position: hit.pose.translation,
       rotation: hit.pose.rotation,
-      scale: vector.Vector3.all(hit.distance * 0.01),
+      // scale: vector.Vector3.all(hit.distance * 0.01),
     );
 
     arCoreController?.addArCoreNodeWithAnchor(earthShape);
